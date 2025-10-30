00d0080b9908cda51a90035dcf04c9c769a67c0d
ral6h <ral6hwastaken@proton.me>
2025-10-30 15:03:15 +0100

    hardcoded pong

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![allow(unused_imports)]
+use std::io::Read;
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -10,14 +12,45 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     loop {
-        let stream = listener.accept();
-        match stream {
-            Ok((_, addr)) => {
-                println!("accepted new connection from host {:?} and port {}", addr.ip(), addr.port());
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+        match listener.accept() {
+            Ok((stream, addr)) => handle_connection(stream, addr).unwrap(),
+            Err(e) => println!("error: {}", e),
         }
     }
+}
+
+const BUFFER_SIZE: usize = 1024;
+
+fn handle_connection(
+    mut stream: std::net::TcpStream,
+    addr: std::net::SocketAddr,
+) -> std::io::Result<()> {
+    println!(
+        "accepted new connection from host {:?} and port {}",
+        addr.ip(),
+        addr.port()
+    );
+
+    'MAIN_LOOP: loop {
+        let mut buffer = [0; BUFFER_SIZE];
+        let mut cmd = String::new();
+
+        'READ_LOOP: loop {
+            let read = stream.read(&mut buffer)?;
+            if let Ok(chunk) = String::from_utf8(buffer[0..read].to_vec()) {
+                println!("read chunk: {chunk}");
+                cmd.push_str(chunk.as_str());
+                if read < BUFFER_SIZE {
+                    break 'READ_LOOP;
+                }
+            } else {
+                break 'MAIN_LOOP;
+            }
+        }
+
+        println!("read raw command {cmd}");
+        stream.write("+PONG\r\n".as_bytes())?;
+
+    }
+    Ok(())
 }
