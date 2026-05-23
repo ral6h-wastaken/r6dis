df481f0b92803918d678f2fa6b0b0d80a9254156
ral6h <ral6hwastaken@proton.me>
2026-05-23 17:09:15 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
-fn main() {
+fn main() -> Result<(), anyhow::Error> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // let msg = "Logs from your program will appear here!";
     // println!("{msg}");
@@ -12,12 +12,17 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                if let Err(err) = stream.write(b"+PONG\r\n") {
+                    anyhow::bail!("Error while writing back response {}", err )
+                }
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
